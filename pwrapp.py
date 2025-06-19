c5149fd6bfba7b9756e3de46463e895d325cdf14
Gourav Kumar Singh <singhdhruwkumar2006@gmail.com>
2025-06-19 19:34:21 +0530

    Update pwrapp.py
    h

--- pwrapp.py
+++ pwrapp.py
@@ -501,13 +501,7 @@ def main():
         layout="wide",
         initial_sidebar_state="expanded"
     )
-    hide_streamlit_style = """
-        <style>
-        #MainMenu {visibility: hidden;}
-        footer {visibility: hidden;}
-        </style>
-        """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+ 
     
     # Custom CSS
     st.markdown("""
